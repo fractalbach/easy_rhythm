b5e19aeee3a021c5f7c3191e4c2017b050e3cc80
fractalbach <fractalbach@gmail.com>
2021-08-30 20:44:42 -0700

    simplified collisions and changed the size of images

--- src/Note/Note.gd
+++ src/Note/Note.gd
@@ -6,10 +6,6 @@ var FALL_SPEED = 300 # 250
 var DESTROY_Y_VALUE = 600
 
 
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _process(delta: float) -> void:
 	position.y = position.y + FALL_SPEED * delta
 	if position.y > DESTROY_Y_VALUE:
--- src/PianoKey/PianoKey.gd
+++ src/PianoKey/PianoKey.gd
@@ -1,57 +1,49 @@
 class_name PianoKey
 extends AnimatedSprite
 
+signal note_hit(type_of_hit)
+
 const BAD_HIT = 0
 const GOOD_HIT = 1
 const PERFECT_HIT = 2
 
-const PERFECT_HIT_LAYER = 1
-const GOOD_HIT_LAYER = 2
-
-const BUFFER_PERFECT_HITS = 20
-const BUFFER_GOOD_HITS = 50
-const BUFFER_OK_HITS = 100
-
-
-signal note_hit(type_of_hit)
-
 var action_name = ""
+
 
 func set_action_name(name):
 	action_name = name
 
+
 func _ready():
 	play("default")
+
 
 func _input(event: InputEvent) -> void:
 	if action_name == "":
 		return
 	if event.is_action_pressed(action_name):
 		handle_note_hit()
-	if event.is_action_released(action_name):
+	elif event.is_action_released(action_name):
 		play("default")
 
 
 func handle_note_hit() -> void:
 	var areas = $Area2D.get_overlapping_areas()
-	# print(areas)
 	if len(areas) > 0:
 		var area = areas[0]
-		print(round(area.owner.position.y - position.y))
-		
-		
-		if area.get_collision_layer_bit(PERFECT_HIT_LAYER):
-			print("perfect hit")
-			play("hit")
-			emit_signal("note_hit", PERFECT_HIT)
-			area.owner.queue_free()
-			return
-		elif area.get_collision_layer_bit(GOOD_HIT_LAYER):
-			print("good hit")
-			play("hit")
-			emit_signal("note_hit", GOOD_HIT)
-			area.owner.queue_free()
-			return
-	print("bad hit!")
+		var dist = abs(round(area.owner.position.y - position.y))
+		if dist <= 15:
+			consume_note(area, PERFECT_HIT)
+		else:
+			consume_note(area, GOOD_HIT)
+		return
 	play("miss")
 	emit_signal("note_hit", BAD_HIT)
+
+
+func consume_note(area, hit_type):
+	play("hit")
+	emit_signal("note_hit", hit_type)
+	area.owner.queue_free()
+
+
