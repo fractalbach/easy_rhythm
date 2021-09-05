34f627c958f359d31bc47673f536eca39f071a99
fractalbach <fractalbach@gmail.com>
2021-09-05 14:40:31 -0700

    now can extract information about each midi message

--- tools/convert_midi/main.py
+++ tools/convert_midi/main.py
@@ -1,21 +1,38 @@
 import argparse
 import pathlib
 import sys
-from mido import MidiFile
+from mido import MidiFile, messages
 
 HELP_MAIN = 'Convert MIDI file into a useful format for EasyRhythm game.'
 HELP_FILEPATH = 'filepath of the midi file to convert'
 
-parser = argparse.ArgumentParser(description=HELP_MAIN)
-parser.add_argument('filepath', help=HELP_FILEPATH, type=pathlib.Path)
-args = parser.parse_args()
-filepath = args.filepath
 
-print(args.filepath, type(args.filepath))
+def main():
+	parser = argparse.ArgumentParser(description=HELP_MAIN)
+	parser.add_argument('filepath', help=HELP_FILEPATH, type=pathlib.Path)
+	args = parser.parse_args()
+	filepath = args.filepath
+	try:
+		parse(filepath)
+	except Exception as e:
+		print(f'Error while parsing the midi file.\nMIDO Error: {e}', file=sys.stderr)
 
-try:
-	mid = MidiFile(filepath)
-	print(mid)
-except Exception as e:
-	print(f'Error while parsing the midi file.\nMIDO Error: {e}', file=sys.stderr)
 
+def parse(filepath):
+	time = 0
+	midi = MidiFile(filepath)
+	for track in midi.tracks:
+		for message in track:
+			time = handle_message(message, time)
+
+
+def handle_message(message, time):
+	if not message.is_meta:
+		if message.type =='note_on' or message.type == 'note_off':
+			mtype = message.type[5:].upper()
+			print(f'{time:6d} : {mtype:3s} : {message.note}')
+	return time + message.time
+
+
+if __name__ == '__main__':
+	main()
