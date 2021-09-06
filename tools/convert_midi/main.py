0d963f1753e8f4e6380d83d187735c99447cf438
fractalbach <fractalbach@gmail.com>
2021-09-05 20:47:12 -0700

    added a simpler way to run the tool

--- tools/convert_midi/main.py
+++ tools/convert_midi/main.py
@@ -1,7 +1,9 @@
 import argparse
 import pathlib
 import sys
+from pprint import pprint
 from mido import MidiFile
+
 
 HELP_MAIN = 'Convert MIDI file into a useful format for EasyRhythm game.'
 HELP_FILEPATH = 'filepath of the midi file to convert'
@@ -13,26 +15,44 @@ def main():
 	args = parser.parse_args()
 	filepath = args.filepath
 	try:
-		parse(filepath)
+		converter = Converter(filepath)
+		converter.display()
 	except Exception as e:
 		print(f'Error while parsing the midi file.\nMIDO Error: {e}', file=sys.stderr)
 
 
-def parse(filepath):
-	time = 0
-	midi = MidiFile(filepath)
-	for track in midi.tracks:
-		for message in track:
-			time = handle_message(message, time)
+class Converter():
+	def __init__(self, filepath):
+		self.note_counter = {}
+		self.time = 0
+		self.output = ''
+		midi = MidiFile(filepath)
+		for track in midi.tracks:
+			for message in track:
+				self.handle_message(message)
 
 
-def handle_message(message, time):
-	if not message.is_meta:
-		if message.type =='note_on' or message.type == 'note_off':
-			mtype = message.type[5:].upper()
-			print(f'{time:6d} : {mtype:3s} : {message.note}')
-	return time + message.time
+	def handle_message(self, message):
+		if message.is_meta:
+			print(message)
+		if not message.is_meta:
+			if message.type =='note_on' or message.type == 'note_off':
+				mtype = message.type[5:].upper()
+				self.output += f'{self.time:6d} : {mtype:3s} : {message.note}'
+				self.count_note(message.note)
+		self.time += message.time
 
+
+	def count_note(self, note):
+		if not note in self.note_counter:
+			self.note_counter[note] = 1
+		else:
+			self.note_counter[note] += 1
+
+
+	def display(self):
+		pprint(self.note_counter)
+		
 
 if __name__ == '__main__':
 	main()
